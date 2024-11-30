5e655646ce3759308645ce2d54ec97c0a5fc1551
Cytezera <ezrachin05@gmail.com>
2024-11-30 13:59:18 +0800

    TicTacToe game complete

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,15 +11,17 @@ void restartBoard(vector<vector<string>> &board){
 }		
 
 void displayBoard(vector<vector<string>> board){
+	cout << "  0 1 2" << endl; 
 	for (int i=0; i < 3 ; i ++) {
+		cout << i << " " ; 
       		for (int j=0; j <3 ; j ++ ) {
 			cout << board[i][j];
 			if ( j < 2) cout << "|"  ; 
 		}	
 		cout << "\n"; 
-		if (i < 2 ) cout << "-----" << endl ; 
+		if (i < 2 ) cout << "  -----" << endl ; 
 	} 
-
+	cout << "\n"; 
 }
 
 void chooseTurn(string& turn){
@@ -32,26 +34,70 @@ void chooseTurn(string& turn){
 }
 
 void placeTile(string turn, vector<vector<string>> &board){
+	int row, col; 	
+	bool check; 
 	do { 
 		cout << "It is " << turn << "'s turn" << endl; 
 		cout << "Please select the row you would like to place : " << endl; 
 		cin >> row; 
 		cout << "Please select the column you would like to place : " << endl; 
-		cin >> col; 
-	}while(		
+		cin >> col;
+	       	if (board[row][col] != " ") {
+			cout << "Invalid input, please try again" << endl; 
+			check = false; 	
+		}else { 
+			board[row][col] = turn; 
+			check = true ;			
+		} 	
+	}while(!check);		
 
 
 }
 
+void checkResult (bool& gamerun, vector<vector<string>> board, string turn) {
+	
+	if ((board[0][0] == board[1][1] && board[1][1] == turn && board[2][2] == turn ) || ( board[0][2] == board[1][1] && board[1][1] == turn && board[2][0] == turn )) {
+
+		displayBoard(board); 
+		cout << turn << " wins " << endl;
+	        gamerun = false; 
+       		return; 		       
+	}	
+
+	for (int i = 0 ; i < 3; i ++ ) {
+		if ((board[i][0] == board[i][1] && board[i][1] == turn &&  board[i][2] == turn ) || (board[0][i]==board[1][i] && board[1][i] == turn && board[2][i] == turn )) { 
+
+			displayBoard(board); 
+			cout << turn <<  " wins " << endl; 
+			gamerun = false;
+			return ;	
+			
+		}	
+		
+		
+	}
+	for (int i = 0 ; i < 3 ; i ++) { 
+		for (int j = 0; j < 3 ; j++) {
+			if (board[i][j] == " " ) {
+				return; 
+			}
+		}
+	} 
+	displayBoard(board);	
+	cout << "It is a draw " << endl; 
+	gamerun = false; 
+	return ;
+} 
 void ticTacToe(){
 	vector<vector<string>> board(3,vector<string>(3)); 
 	restartBoard(board); 
 	bool gamerun = true; 
-	string turn = "X"; 
+	string turn = "O"; 
 	while (gamerun){
-		displayBoard(board); 	
 		chooseTurn(turn); 
-		placeTile(turn); 			
+		displayBoard(board); 	
+		placeTile(turn, board); 			
+		checkResult(gamerun,board,turn);	
 	};
 
 }
@@ -73,10 +119,10 @@ int main()
 				break; 
 			case 2: 
 				cout << "Goodbye" << endl; 
-				return 0; 
+				break; 
 			default: 
 				cout << "Invalid input" ; 
-
+				cin.clear();	
 		}
 		
 	
@@ -85,5 +131,5 @@ int main()
 		}while(userInput !=2) ;	
 
 
-
+		return 0 ; 
 	}
